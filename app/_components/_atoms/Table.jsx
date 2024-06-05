d2cf303afb748e1989f8626199d19e471c86c356
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2024-06-05 12:48:39 +0100

    table download as excel is handled for policy/lagislation

--- app/_components/_atoms/Table.jsx
+++ app/_components/_atoms/Table.jsx
@@ -1,25 +1,40 @@
+"use client";
+import React, { useRef } from "react";
+import { useDownloadExcel } from "react-export-table-to-excel";
 import Link from "next/link";
 import { ExternalLink } from "lucide-react";
+import Button from "./Button";
+
+// import DownloadExcel from "./DownloadExcel";
 
 const Table = ({ policies, headers }) => {
+  const tableRef = useRef(null);
+
+  const { onDownload } = useDownloadExcel({
+    currentTableRef: tableRef.current,
+    filename: "GAPs Policy Lagislation",
+    sheet: "Countries",
+  });
+
   return policies.length === 0 ? (
     "Plase select a Country, Start Year, End Year or enter a policy term or simply click the Search button to list all the policies..."
   ) : (
     <div className="overflow-x-auto">
       <div className="w-full overflow-x-auto ">
-        <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
-          <thead className="ltr:text-left rtl:text-right ">
-            <tr>
-              {headers.map((header, index) => (
-                <th
-                  key={index}
-                  className="whitespace-nowrap px-4 py-2 font-bold text-gray-900"
-                >
-                  {header.label}
-                </th>
-              ))}
-            </tr>
-          </thead>
+        <table
+          ref={tableRef}
+          className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm"
+        >
+          <tr>
+            {headers.map((header, index) => (
+              <th
+                key={index}
+                className="whitespace-nowrap px-4 py-2 font-bold text-gray-900"
+              >
+                {header.label}
+              </th>
+            ))}
+          </tr>
 
           <tbody className="divide-y divide-gray-200">
             {policies.map((policy, index) => (
@@ -106,6 +121,7 @@ const Table = ({ policies, headers }) => {
             ))}
           </tbody>
         </table>
+        <Button label={"Download as Excel"} onClick={onDownload} icon={false} />
       </div>
     </div>
   );
