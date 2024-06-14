d29fc6b60f0a0b573c8985bee65de9b8ffe5dd62
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2024-06-14 14:33:12 +0100

    api links changed

--- app/_utils/GlobalApi.jsx
+++ app/_utils/GlobalApi.jsx
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000/api";
+const BASE_URL = "https://gap-project-eight.vercel.app/api";
 
 const fetchData = async (endpoint, options = {}) => {
   const defaultOptions = {
