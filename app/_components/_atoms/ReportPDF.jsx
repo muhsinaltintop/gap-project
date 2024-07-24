68aa9d625f99f4a3e53180da99c9047ceaf7f6dd
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2024-07-24 16:49:05 +0100

    pdf can be downloaded properly with footer

--- app/_components/_atoms/ReportPDF.jsx
+++ app/_components/_atoms/ReportPDF.jsx
@@ -28,7 +28,6 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
   },
   tableCol: {
-    width: 'auto',
     borderStyle: 'solid',
     borderColor: '#000',
     borderWidth: 1,
@@ -39,14 +38,14 @@ const styles = StyleSheet.create({
     borderStyle: 'solid',
     borderColor: '#000',
     borderWidth: 1,
-    flex: 1,
     textAlign: 'left', // Align text to left
   },
   header: {
     fontWeight: 'bold',
-    textAlign: 'center', // Center align header text
+    textAlign: 'center',// Center align header text
   },
   headerCell: {
+    backgroundColor: '#e0e0e0',
     fontSize: '10px',
     padding: 5,
     borderStyle: 'solid',
@@ -55,8 +54,29 @@ const styles = StyleSheet.create({
   },
   cellText: {
     margin: 5,
-    fontSize: '9px'
+    fontSize: '9px',
   },
+  footer: {
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
+    right: 20,
+    textAlign: 'center',
+    fontSize: 8,
+    borderTopWidth: 1,
+    borderTopColor: '#000',
+    paddingTop: 10,
+  },
+
+  col1: { flex: 3 },
+  col2: { flex: 7 },
+  col3: { flex: 7 },
+  col4: { flex: 4 },
+  col5: { flex: 4 },
+  col6: { flex: 4 },
+  col7: { flex: 6 },
+  col8: { flex: 11 },
+  col9: { flex: 8 },
 });
 
 const ReportPDF = ({ policies, headers }) => {
@@ -77,14 +97,19 @@ const ReportPDF = ({ policies, headers }) => {
     return formatted.trim().replace(/,$/, ""); // Remove trailing comma
   };
 
+  const getCurrentDate = () => {
+    const now = new Date();
+    return `${now.getDate()}/${now.getMonth() + 1}/${now.getFullYear()}`;
+  };
+
   return (
     <Document>
       <Page style={styles.page} orientation='landscape'>
         <View style={styles.table}>
-          {/* Header Row */}
+          
           <View style={styles.tableRow}>
             {headers?.map((header, indexHeader) => (
-              <View key={indexHeader} style={[styles.tableCell, styles.headerCell]}>
+              <View key={indexHeader} style={[styles.tableCell, styles.headerCell, styles[`col${indexHeader + 1}`]]}>
                 <Text style={styles.header}>{header?.label}</Text>
               </View>
             ))}
@@ -93,36 +118,37 @@ const ReportPDF = ({ policies, headers }) => {
           {/* Data Rows */}
           {policies.map((policy, index) => (
             <View key={index} style={styles.tableRow}>
-              <View style={styles.tableCell}>
+              <View style={[styles.tableCell, styles.col1]}>
                 <Text style={styles.cellText}>{policy.country}</Text>
               </View>
-              <View style={styles.tableCell}>
+              <View style={[styles.tableCell, styles.col2]}>
                 <Text style={styles.cellText}>{policy.policyName}</Text>
               </View>
-              <View style={styles.tableCell}>
+              <View style={[styles.tableCell, styles.col3]}>
                 <Text style={policy.country === "Greece" ? [styles.cellText, styles.greek] : styles.cellText}>{policy.originalPolicyName}</Text>
               </View>
-              <View style={styles.tableCell}>
+              <View style={[styles.tableCell, styles.col4]}>
                 <Text style={styles.cellText}>{`${policy.day}/${policy.month}/${policy.year}`}</Text>
               </View>
-              <View style={styles.tableCell}>
+              <View style={[styles.tableCell, styles.col5]}>
                 <Text style={styles.cellText}>{policy.typeOfLegislation}</Text>
               </View>
-              <View style={styles.tableCell}>
+              <View style={[styles.tableCell, styles.col6]}>
                 <Text style={styles.cellText}>{policy.levelOfLegislation}</Text>
               </View>
-              <View style={styles.tableCell}>
+              <View style={[styles.tableCell, styles.col7]}>
                 <Text style={styles.cellText}>{formatPolicyTypeArea(policy.policyTypeArea)}</Text>
               </View>
-              <View style={styles.tableCell}>
+              <View style={[styles.tableCell, styles.col8]}>
                 <Text style={styles.cellText}>{policy.policyDescription}</Text>
               </View>
-              <View style={styles.tableCell}>
+              <View style={[styles.tableCell, styles.col9]}>
                 <Text style={styles.cellText}>{policy.notes}</Text>
               </View>
             </View>
           ))}
         </View>
+        <Text style={styles.footer}>Document downloaded on {getCurrentDate()} from https://www.returnmigration.eu/</Text>
       </Page>
     </Document>
   );
