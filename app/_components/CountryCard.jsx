533afc768097e474d4146ea5fe45a8b385fdcabb
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2024-08-07 19:00:03 +0100

    country list statistics dropdown added

--- app/_components/CountryCard.jsx
+++ app/_components/CountryCard.jsx
@@ -1,153 +1,176 @@
+"use client"
 import Link from "next/link";
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Image from "next/image";
 
 const CountryCard = ({ country }) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setIsDropdownOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className="flex items-end gap-4 m-4">
-  <Image
-    src={`/${country.countryName}.png`}
-    width={500}
-    height={64}
-    alt=""
-    className="size-36 rounded-full object-cover bg-slate-200 p-4"
-  />
+    <div className="flex items-end gap-4 m-4 relative"> {/* Added relative positioning here */}
+      <Image
+        src={`/${country.countryName}.png`}
+        width={500}
+        height={64}
+        alt=""
+        className="size-36 rounded-full object-cover bg-slate-200 p-4"
+      />
 
-  <div>
-    <h3 className="text-lg/tight font-medium text-gray-900">{country.countryName}</h3>
+      <div>
+        <h3 className="text-lg/tight font-medium text-gray-900">{country.countryName}</h3>
 
-    <div className="grid grid-cols-4 gap-x-4 mt-0.5 text-gray-700 ">
-        <Link
-          href={
-            ["Greece", "Iraq", "Morocco", "Poland", "Sweden"].includes(
-              country?.countryName
-            )
-              ? `https://www.returnmigration.eu/countries_${country?.countryName.toLowerCase()}`
-              : `https://www.returnmigration.eu/countries-${country?.countryName.toLowerCase()}`
-          }
-          target="_blank"
-          className="group relative block overflow-hidden"
-        >
-          <button className="block w-full rounded bg-yellow-400 p-4 mt-2 text-sm font-medium transition hover:scale-105">
-            Country Profile On GAPs
-          </button>
-        </Link>
-        <Link
-          href={`/return-policy-timeline/${country?.countryName}`}
-          target="_blank"
-          className="group relative block overflow-hidden"
-        >
-          <button className="block w-full rounded bg-yellow-400 p-4 mt-2 text-sm font-medium transition hover:scale-105">
-            Return Policy Timeline
-          </button>
-        </Link>
-        <Link
-          target="_blank"
-          href={`/return-flow-chart/${country?.countryName}`}
-          className="group relative block overflow-hidden"
-        >
-          <button className="block w-full rounded bg-yellow-400 p-4 mt-2 text-sm font-medium transition hover:scale-105">
-            Return Flow Chart
-          </button>
-        </Link>
-        <Link
-          target="_blank"
-          href={`/actor-return-diagram/${country?.countryName}`}
-          className="group relative block overflow-hidden"
-        >
-          <button className="block w-full rounded bg-yellow-400 p-4 mt-2 text-sm font-medium transition hover:scale-105">
-            Actor Return Diagram
-          </button>
-        </Link>
-        <Link
-          target="_blank"
-          href={`/policy-legislation-map/`}
-          className="group relative block overflow-hidden"
-        >
-          <button className="block w-full rounded bg-yellow-400 p-4 mt-2 text-sm font-medium transition hover:scale-105">
-            Policy/Legislation
-          </button>
-        </Link>
-        <Link
-          target="_blank"
-          href={`/return-infrastructure`}
-          className="group relative block overflow-hidden"
-        >
-          <button className="block w-full rounded bg-yellow-400 p-4 mt-2 text-sm font-medium transition hover:scale-105">
-          Return Infrastructure
-          </button>
-        </Link>
-        <Link
-          target="_blank"
-          href={`/statistics`}
-          className="group relative block overflow-hidden"
-        >
-          <button className="block w-full rounded bg-yellow-400 p-4 mt-2 text-sm font-medium transition hover:scale-105">
-            Statistics
-          </button>
-        </Link>
+        <div className="grid grid-cols-4 gap-x-4 mt-0.5 text-gray-700">
+          <Link
+            href={
+              ["Greece", "Iraq", "Morocco", "Poland", "Sweden"].includes(
+                country?.countryName
+              )
+                ? `https://www.returnmigration.eu/countries_${country?.countryName.toLowerCase()}`
+                : `https://www.returnmigration.eu/countries-${country?.countryName.toLowerCase()}`
+            }
+            target="_blank"
+            className="group relative block overflow-hidden"
+          >
+            <button className="block w-full rounded bg-yellow-400 p-4 mt-2 text-sm font-medium transition hover:scale-105">
+              Country Profile On GAPs
+            </button>
+          </Link>
+          <Link
+            href={`/return-policy-timeline/${country?.countryName}`}
+            target="_blank"
+            className="group relative block overflow-hidden"
+          >
+            <button className="block w-full rounded bg-yellow-400 p-4 mt-2 text-sm font-medium transition hover:scale-105">
+              Return Policy Timeline
+            </button>
+          </Link>
+          <Link
+            target="_blank"
+            href={`/return-flow-chart/${country?.countryName}`}
+            className="group relative block overflow-hidden"
+          >
+            <button className="block w-full rounded bg-yellow-400 p-4 mt-2 text-sm font-medium transition hover:scale-105">
+              Return Flow Chart
+            </button>
+          </Link>
+          <Link
+            target="_blank"
+            href={`/actor-return-diagram/${country?.countryName}`}
+            className="group relative block overflow-hidden"
+          >
+            <button className="block w-full rounded bg-yellow-400 p-4 mt-2 text-sm font-medium transition hover:scale-105">
+              Actor Return Diagram
+            </button>
+          </Link>
+          <Link
+            target="_blank"
+            href={`/policy-legislation-map/`}
+            className="group relative block overflow-hidden"
+          >
+            <button className="block w-full rounded bg-yellow-400 p-4 mt-2 text-sm font-medium transition hover:scale-105">
+              Policy/Legislation
+            </button>
+          </Link>
+          <Link
+            target="_blank"
+            href={`/return-infrastructure`}
+            className="group relative block overflow-hidden"
+          >
+            <button className="block w-full rounded bg-yellow-400 p-4 mt-2 text-sm font-medium transition hover:scale-105">
+              Return Infrastructure
+            </button>
+          </Link>
+          <div className="relative" ref={dropdownRef}> {/* Ensure this has relative positioning and ref */}
+            <button
+              className="block w-full rounded bg-yellow-400 p-4 mt-2 text-sm font-medium transition hover:scale-105"
+              onClick={toggleDropdown}
+            >
+              Statistics
+            </button>
+            {isDropdownOpen && (
+              <div className="absolute mt-2 w-full rounded bg-white shadow-lg z-50"> {/* Added z-50 here */}
+                <Link
+                  href={`/statistics/#`}
+                  target="_blank"
+                  className="block w-full p-2 text-sm text-gray-700 hover:bg-gray-100"
+                >
+                  Stock of Irregular Migrant
+                </Link>
+                <Link
+                  href={`/statistics/#`}
+                  target="_blank"
+                  className="block w-full p-2 text-sm text-gray-700 hover:bg-gray-100"
+                >
+                  Asylum Application
+                </Link>
+                <Link
+                  href={`/statistics/#`}
+                  target="_blank"
+                  className="block w-full p-2 text-sm text-gray-700 hover:bg-gray-100"
+                >
+                  Entry Refusals and Pushbacks
+                </Link>
+                <Link
+                  href={`/statistics/dublin-returns`}
+                  target="_blank"
+                  className="block w-full p-2 text-sm text-gray-700 hover:bg-gray-100"
+                >
+                  Dublin Returns
+                </Link><Link
+                  href={`/statistics/#`}
+                  target="_blank"
+                  className="block w-full p-2 text-sm text-gray-700 hover:bg-gray-100"
+                >
+                  TCNs/Foreign Nationals Ordered to Leave
+                </Link><Link
+                  href={`/statistics/#`}
+                  target="_blank"
+                  className="block w-full p-2 text-sm text-gray-700 hover:bg-gray-100"
+                >
+                  Returns By Type
+                </Link><Link
+                  href={`/statistics/#`}
+                  target="_blank"
+                  className="block w-full p-2 text-sm text-gray-700 hover:bg-gray-100"
+                >
+                  Returns By Citizenship
+                </Link><Link
+                  href={`/statistics/#`}
+                  target="_blank"
+                  className="block w-full p-2 text-sm text-gray-700 hover:bg-gray-100"
+                >
+                  Alternative/Various Categories(National)
+                </Link><Link
+                  href={`/statistics/#`}
+                  target="_blank"
+                  className="block w-full p-2 text-sm text-gray-700 hover:bg-gray-100"
+                >
+                  Readmitted Citizens
+                </Link>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-
-    // <div className="country-card">
-    //   <Image
-    //     src={`/${country.countryName}.png`}
-    //     alt={country.countryName}
-    //     className="h-64 w-full object-cover transition duration-500 group-hover:scale-105 sm:h-72"
-    //     width={500}
-    //     height={64}
-    //   />
-
-    //   <div className="relative border border-gray-100 bg-white p-6">
-    //     <h2 className="text-2xl font-bold text-center">
-    //       {country.countryName}
-    //     </h2>
-
-    //     <Link
-    //       href={
-    //         ["Greece", "Iraq", "Morocco", "Poland", "Sweden"].includes(
-    //           country?.countryName
-    //         )
-    //           ? `https://www.returnmigration.eu/countries_${country?.countryName.toLowerCase()}`
-    //           : `https://www.returnmigration.eu/countries-${country?.countryName.toLowerCase()}`
-    //       }
-    //       target="_blank"
-    //       className="group relative block overflow-hidden"
-    //     >
-    //       <button className="block w-full rounded bg-yellow-400 p-4 mt-2 text-sm font-medium transition hover:scale-105">
-    //         Country Profile On GAPs
-    //       </button>
-    //     </Link>
-    //     <Link
-    //       href={`/return-policy-timeline/${country?.countryName}`}
-    //       target="_blank"
-    //       className="group relative block overflow-hidden"
-    //     >
-    //       <button className="block w-full rounded bg-yellow-400 p-4 mt-2 text-sm font-medium transition hover:scale-105">
-    //         Return Policy Timeline
-    //       </button>
-    //     </Link>
-    //     <Link
-    //       target="_blank"
-    //       href={`/return-flow-chart/${country?.countryName}`}
-    //       className="group relative block overflow-hidden"
-    //     >
-    //       <button className="block w-full rounded bg-yellow-400 p-4 mt-2 text-sm font-medium transition hover:scale-105">
-    //         Return Flow Chart
-    //       </button>
-    //     </Link>
-    //     <Link
-    //       target="_blank"
-    //       href={`/actor-return-diagram/${country?.countryName}`}
-    //       className="group relative block overflow-hidden"
-    //     >
-    //       <button className="block w-full rounded bg-yellow-400 p-4 mt-2 text-sm font-medium transition hover:scale-105">
-    //         Actor Return Diagram
-    //       </button>
-    //     </Link>
-    //   </div>
-    // </div>
   );
 };
 
