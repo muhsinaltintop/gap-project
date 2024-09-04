392b9813152729a6df72302d63b2a8408a589881
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2024-09-04 10:49:25 +0100

    cors added to route

--- app/api/returns-by-citizenship-gr/route.js
+++ app/api/returns-by-citizenship-gr/route.js
@@ -3,17 +3,33 @@ import pool from "@/lib/mysql";
 
 export async function GET(request) {
   try {
+    const origin = request.headers.get("origin");
+    const allowedOrigins = ['https://gap-project-eight.vercel.app', 'http://localhost:3000'];
+
+    // CORS kontrolü: sadece izin verilen kökenlere izin veriyoruz
+    if (!allowedOrigins.includes(origin)) {
+      return new NextResponse('CORS policy: This origin is not allowed', { status: 403 });
+    }
+
     const db = await pool.getConnection();
 
     const query = "select * from rbc_greece";
     const [rows] = await db.execute(query);
     db.release();
 
-    return NextResponse.json(rows);
+    const response = NextResponse.json(rows);
+
+    // CORS başlıklarını yanıtımıza ekliyoruz
+    response.headers.set('Access-Control-Allow-Origin', origin);
+    response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    response.headers.set('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+    response.headers.set('Access-Control-Allow-Credentials', 'true');
+
+    return response;
   } catch (error) {
     return NextResponse.json(
       {
-        error: error,
+        error: error.message || "Internal Server Error",
       },
       { status: 500 }
     );
