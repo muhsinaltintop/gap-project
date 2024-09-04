8a5d04ed1f103695a65741037f57153d5b12546b
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2024-09-04 13:25:47 +0100

    cors

--- app/api/returns-by-citizenship-ge/route.js
+++ app/api/returns-by-citizenship-ge/route.js
@@ -3,33 +3,17 @@ import pool from "@/lib/mysql";
 
 export async function GET(request) {
   try {
-    const origin = request.headers.get("origin");
-    const allowedOrigins = ['*'];
-
-    // CORS kontrolü: sadece izin verilen kökenlere izin veriyoruz
-    if (!allowedOrigins.includes(origin)) {
-      return new NextResponse('CORS policy: This origin is not allowed', { status: 403 });
-    }
-
     const db = await pool.getConnection();
 
     const query = "select * from rbc_germany";
     const [rows] = await db.execute(query);
     db.release();
 
-    const response = NextResponse.json(rows);
-
-    // CORS başlıklarını yanıtımıza ekliyoruz
-    response.headers.set('Access-Control-Allow-Origin', origin);
-    response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    response.headers.set('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
-    response.headers.set('Access-Control-Allow-Credentials', 'true');
-
-    return response;
+    return NextResponse.json(rows);
   } catch (error) {
     return NextResponse.json(
       {
-        error: error.message || "Internal Server Error",
+        error: error,
       },
       { status: 500 }
     );
