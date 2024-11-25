12e05c9b9edf68af7c02eb25ba4f26edaf1d642e
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2024-11-25 12:21:33 +0000

    tcn_desicion_for_irregular api call table changed

--- app/api/tcn-return-desicion-for-irregulars/route.js
+++ app/api/tcn-return-desicion-for-irregulars/route.js
@@ -5,7 +5,7 @@ export async function GET(request) {
   try {
     const db = await pool.getConnection();
 
-    const query = "select * from returnDesicionForIrregulars";
+    const query = "select * from tcn_desicion_for_irregular";
     const [rows] = await db.execute(query);
     db.release();
 
