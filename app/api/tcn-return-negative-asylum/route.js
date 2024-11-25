4776ee735dc2522c75376fdf56653018de2c16d2
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2024-11-25 13:47:38 +0000

    tcn_negative_asylum updated

--- app/api/tcn-return-negative-asylum/route.js
+++ app/api/tcn-return-negative-asylum/route.js
@@ -5,7 +5,7 @@ export async function GET(request) {
   try {
     const db = await pool.getConnection();
 
-    const query = "select * from returnNegativeAsylum";
+    const query = "select * from tcn_negative_asylum";
     const [rows] = await db.execute(query);
     db.release();
 
