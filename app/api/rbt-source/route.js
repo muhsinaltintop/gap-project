61cf296a8af367df80cf2041cfb5328fad50744f
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2024-11-26 14:49:49 +0000

    rbt_source updated

--- app/api/rbt-source/route.js
+++ app/api/rbt-source/route.js
@@ -5,7 +5,7 @@ export async function GET(request) {
   try {
     const db = await pool.getConnection();
 
-    const query = "select * from rbtSource";
+    const query = "select * from rbt_source";
     const [rows] = await db.execute(query);
     db.release();
 
