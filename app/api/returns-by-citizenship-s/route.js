02e0bf3dd545c9296dba2c6a6472a7282e222208
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2024-11-27 15:05:13 +0000

    sweden added to rbc

---
+++ app/api/returns-by-citizenship-s/route.js
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import pool from "@/lib/mysql";
+
+export async function GET(request) {
+  try {
+    const db = await pool.getConnection();
+
+    // URL parametrelerini alıyoruz
+    const { searchParams } = new URL(request.url);
+    const year = searchParams.get('year');
+
+    // Yıl parametresi varsa, sorguya ekleyelim
+    let query = "SELECT * FROM rbc_sweden";
+    if (year) {
+      query += ` WHERE year = ?`;
+    }
+
+    // Veritabanı sorgusunu çalıştır
+    const [rows] = await db.execute(query, year ? [year] : []);
+    db.release();
+
+    // Sonuçları döndür
+    return NextResponse.json(rows);
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: error.message,
+      },
+      { status: 500 }
+    );
+  }
+}
