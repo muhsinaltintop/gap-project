b6f2231c1d12f33b222e915ac4a607db131c293a
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2024-12-26 14:25:16 +0000

    links shortened to Link

--- app/_components/_atoms/PlainTable.jsx
+++ app/_components/_atoms/PlainTable.jsx
@@ -23,6 +23,9 @@ const PlainTable = ({ policies, headersData, pathName, selectedCountries }) => {
                 >
                     <thead>
                         <tr>
+                            {console.log("headers:", headers)}
+                            {console.log("policies:", policies)}
+
                             {headers.map((header, index) => (
                                 <th
                                 key={index}
@@ -42,51 +45,57 @@ const PlainTable = ({ policies, headersData, pathName, selectedCountries }) => {
                         </tr>
                     </thead>
                     <tbody className="divide-y divide-black align-top">
-                        {policies.map((policy, rowIndex) => (
-                            <tr key={rowIndex}>
-                                {headers.map((header, colIndex) => (
-                                    <td
-                                        key={colIndex}
-                                        className='px-4 py-2'
-                                    >
-                                        {header.accessor === 'policyName' && policy.urlEnglish ? (
-                                            <span className="flex items-center">
-                                            {policy.policyName}
-                                            {policy?.urlEnglish ? (
-                                              <Link
-                                                className="ml-2"
-                                                href={`${policy.urlEnglish}`}
-                                                target="_blank"
-                                              >
-                                                <ExternalLink className="size-3" color="#0d7dff" />
-                                              </Link>
-                                            ) : (
-                                              ""
-                                            )}
-                                          </span>
-                                        ) : header.accessor === 'originalPolicyName' && policy.originalUrl ? (
-                                          <span className="flex items-center">
-                                          {policy.originalPolicyName}
-                                          {policy?.originalUrl ? (
-                                            <Link
-                                              className="ml-2"
-                                              href={`${policy.originalUrl}`}
-                                              target="_blank"
-                                            >
-                                              <ExternalLink className="size-3" color="#0d7dff" />
-                                            </Link>
-                                          ) : (
-                                            ""
-                                          )}
-                                        </span>
-                                        ) : (
-                                            policy[header.accessor] !== undefined ? policy[header.accessor] : ''
-                                        )}
-                                    </td>
-                                ))}
-                            </tr>
-                        ))}
-                    </tbody>
+    {policies.map((policy, rowIndex) => (
+        <tr key={rowIndex}>
+            {headers.map((header, colIndex) => (
+                <td key={colIndex} className='px-4 py-2'>
+                    {header.accessor === 'policyName' && policy.urlEnglish ? (
+                        <span className="flex items-center">
+                            {policy.policyName}
+                            {policy?.urlEnglish ? (
+                                <Link
+                                    className="ml-2"
+                                    href={`${policy.urlEnglish}`}
+                                    target="_blank"
+                                >
+                                    <ExternalLink className="size-3" color="#0d7dff" />
+                                </Link>
+                            ) : (
+                                ""
+                            )}
+                        </span>
+                    ) : header.accessor === 'originalPolicyName' && policy.originalUrl ? (
+                        <span className="flex items-center">
+                            {policy.originalPolicyName}
+                            {policy?.originalUrl ? (
+                                <Link
+                                    className="ml-2"
+                                    href={`${policy.originalUrl}`}
+                                    target="_blank"
+                                >
+                                    <ExternalLink className="size-3" color="#0d7dff" />
+                                </Link>
+                            ) : (
+                                ""
+                            )}
+                        </span>
+                    ) : header.accessor === 'officialWebsite' && policy.officialWebsite ? (
+                        <Link href={policy.officialWebsite} target="_blank" className="text-blue-500 underline">
+                            Link
+                        </Link>
+                    ) : header.accessor === 'programEvaluation' && policy.programEvaluation ? (
+                        <Link href={policy.programEvaluation} target="_blank" className="text-blue-500 underline">
+                            Link
+                        </Link>
+                    )  : (
+                        policy[header.accessor] !== undefined ? policy[header.accessor] : ''
+                    )}
+                </td>
+            ))}
+        </tr>
+    ))}
+</tbody>
+
                 </table>
             </div>
         </div>
